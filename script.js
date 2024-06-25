06af2a0ac328d550b87946a0ae7ebd834523dfbd
Arbaj Ansari <83424578+ArbajAr7@users.noreply.github.com>
2024-06-25 11:38:17 +0545

    created a submission

--- script.js
+++ script.js
@@ -1,53 +1,53 @@
-//your JS code here. If required.
-function saveToCookies(){
-	let font_size = document.getElementById("fontsize").value;
-	let font_color = document.getElementById("fontcolor").value;
-	if(font_size<=8 || font_size>=120)
-	{
-		alert("Please enter Font Size range between 8 - 72");
-	}else{
-		setCookie("Font_Size",font_size,7);
-		setCookie("Font_Color",font_color,7);
-	}
-
-}
-
 function setCookie(name, value, days) {
-    var expires = "";
+    let expires = "";
     if (days) {
-        var date = new Date();
+        const date = new Date();
         date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
         expires = "; expires=" + date.toUTCString();
     }
-    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
 }
-
-// Function to get a cookie
+ 
 function getCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    const nameEQ = name + "=";
+    const ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
     }
     return null;
 }
-
+ 
+// Apply saved preferences from cookies
 function applyPreferences() {
-    var fontSize = getCookie('Font_Size');
-    var fontColor = getCookie('Font_Color');
-
-   if (fontSize) {
-        document.getElementById('fontsize').value = fontSize;
-        document.documentElement.style.setProperty('--fontsize', fontSize + 'px');
+    const savedFontSize = getCookie('fontsize');
+    const savedFontColor = getCookie('fontcolor');
+ 
+    if (savedFontSize) {
+        document.documentElement.style.setProperty('--fontsize', savedFontSize + 'px');
+        document.getElementById('fontsize').value = savedFontSize;
     }
-    if (fontColor) {
-        document.getElementById('fontcolor').value = fontColor;
-        document.documentElement.style.setProperty('--fontcolor', fontColor);
+ 
+    if (savedFontColor) {
+        document.documentElement.style.setProperty('--fontcolor', savedFontColor);
+        document.getElementById('fontcolor').value = savedFontColor;
     }
 }
-
-window.onload = function() {
-    applyPreferences();
-};
+ 
+// Handle form submission to save preferences
+document.getElementById('preferences-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+ 
+    const fontSize = document.getElementById('fontsize').value;
+    const fontColor = document.getElementById('fontcolor').value;
+ 
+    document.documentElement.style.setProperty('--fontsize', fontSize + 'px');
+    document.documentElement.style.setProperty('--fontcolor', fontColor);
+ 
+    setCookie('fontsize', fontSize, 7);
+    setCookie('fontcolor', fontColor, 7);
+});
+ 
+// Apply preferences on page load
+applyPreferences();
